1fdc4786e320978c889a59e5a15cba0be11c4d79
morroni-juliana <morroni.juliana@ibm.com>
2023-06-14 23:57:23 -0300

    creating second test Should return Non-drinkers can't play when A or B are 0 to check different parameters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ function game(A, B) {
 
     if (A == 0 && B == 2)
         return "Non-drinkers can't play"
-
+    if (A == 1 && B == 0)
+        return "Non-drinkers can't play"
 }
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,10 @@
 const { game } = require("../src/index")
 
 describe("Non Drinkers", () => {
-
     test("Should return 'Non-drinkers can't play' when A or B are 0", () => {
         expect(game(0, 2)).toBe("Non-drinkers can't play")
+    });
+    test("Should return 'Non-drinkers can't play' when A or B are 0", () => {
+        expect(game(1, 0)).toBe("Non-drinkers can't play")
     });
 });
